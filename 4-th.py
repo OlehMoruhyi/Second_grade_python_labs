0103873ba51ca789c1172ba74eca688f2b71608c
OlehMoruhyi <82759680+OlehMoruhyi@users.noreply.github.com>
2021-09-16 14:36:42 +0300

    Update 4-th.py

--- 4-th.py
+++ 4-th.py
@@ -8,10 +8,10 @@ for i in range(len(weight)-1, -1, -1):
         break
 backpack = [1] + [0] * max_weight
 for i in weight[::-1]:
-    for k in range(i, max_weight+1):
+    for k in range(max_weight, i-1, -1):
         if backpack[k-i] == 1:
             backpack[k] = 1
-for i in range(max_weight, 0, -1):
+for i in range(max_weight, -1, -1):
     if backpack[i]:
         print(i)
         break
